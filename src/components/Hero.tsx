104eeda5ed39deec59778062ee3c5c0ef63024cd
khalil <khalil.haouas@gmail.com>
2024-10-15 11:05:04 +0200

    finish

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -13,8 +13,8 @@ const Hero = () => {
         </div>
         <div>
           <h1 className={`${styles.heroHeadText} text-white`}>
-            Hi, I'm
-            <span className=" text-[#915eff]">Khalil Haouas</span>
+         <div className=" flex gap-2 items-center"> <span>  Hi</span> <img className=" w-20 h-20 rounded-full" src="./khalilBild.jpg" alt="" />,</div> 
+         I'm <span className=" text-[#915eff]">Khalil Haouas</span>
           </h1>
           <p className={`${styles.heroSubText} mt-2 text-white-100`}>
             I develop , user
