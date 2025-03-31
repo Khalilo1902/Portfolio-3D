e4922de8f047a89350aca8eb8a3c7405a17e5cd2
khalil <khalil.haouas@gmail.com>
2025-03-31 22:26:23 +0200

    update

--- vite.config.ts
+++ vite.config.ts
@@ -2,12 +2,11 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
     hmr: {
-      protocol: 'ws',  // Verwende 'ws' für unsichere WebSocket-Verbindung
+      protocol: 'wss',  // Verwende 'wss' für sichere WebSocket-Verbindung
       host: 'khalil-webdev.de',
       port: 3505,
     },
